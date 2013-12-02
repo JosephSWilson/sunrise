341f23d2df1fb94b82af4cf7d9028e133551d458
Joseph Wilson <joseph.s.wilson@gmail.com>
2013-12-02 07:38:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sunrise::Application.config.secret_key_base = '28aa619721ec3f6239d5b798302a40c837390a4261123f694a019764579098fb58ea82f6202e6e2e17b54400f6cb3840966fdff8227ed19eaf13b3d441337f12'
